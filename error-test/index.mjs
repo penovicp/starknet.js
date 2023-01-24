6fea3ac0d010a29fa40f4edc854d272abb5399b8
Petar Penovic <8030344+penovicp@users.noreply.github.com>
2023-01-24 07:12:12 +0100

    test: instanceof

---
+++ error-test/index.mjs
@@ -0,0 +1,17 @@
+import * as starknet from 'starknet'
+
+import * as beta from 'stjs'
+
+const test = (lib) => {
+    const provider = new lib.SequencerProvider({
+        baseUrl: 'http://127.0.0.1:5050/',
+    })
+
+    provider.getContractAddresses().catch(e => {
+        if (!(e instanceof lib.HttpError))
+            throw new Error(e)
+    })
+}
+
+test(starknet)
+test(beta)
